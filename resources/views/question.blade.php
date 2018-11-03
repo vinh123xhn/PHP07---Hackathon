5310c7068949652119a5738fe0eed05ecb8e64b3
letuan1 <le01627177611@gmail.com>
2018-11-03 11:21:08 +0700

    controller

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -1,4 +1,6 @@
-<form>
-    <input type="number">
+<form action="{{ route('showInput') }}">
+    {{ csrf_field() }}
+    Số nguwofi chơi :
+    <input type="number" name="number">
     <input type="submit">
 </form>
